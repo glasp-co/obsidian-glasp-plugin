6303e45b87e3811d0f3a6dc471a7a944ae5fa3f9
k-nagai-tver <k-nagai@tver.co.jp>
2025-01-13 01:45:00 +0900

    feat: add left bar icon

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Plugin } from "obsidian";
+import { Notice, Plugin, addIcon } from "obsidian";
+import glaspIcon from "src/assets/glasp.svg";
 import { WriteHighlightController } from "./controller";
 import { ObsidianApp } from "./obsidian-api";
 import { SettingTab } from "./setting";
@@ -10,17 +11,26 @@ export default class ObsidianGlaspPlugin extends Plugin {
 		const data = await this.loadData();
 		this.settings = new SettingTab(this, data);
 		this.addSettingTab(this.settings);
+		this.addLeftBarIcon();
 		this.writeHighlights();
 	}
 
 	onunload() {}
 
 	private async writeHighlights() {
+		new Notice("Updating Highlights from Glasp");
 		const obApp = new ObsidianApp(this.app);
 		const controller = new WriteHighlightController(obApp);
 		await controller.run({
 			accessToken: this.settings.value.accessToken,
 			folder: this.settings.value.folder,
+		});
+	}
+
+	private addLeftBarIcon() {
+		addIcon("glasp", glaspIcon);
+		this.addRibbonIcon("glasp", "Glasp Highlights", () => {
+			this.writeHighlights();
 		});
 	}
 }
